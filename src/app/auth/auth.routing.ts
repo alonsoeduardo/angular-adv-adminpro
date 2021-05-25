312bb3a8d896343daa2855f89d2b41af40ff8494
Eduardo <alonsoeduardo@gmail.com>
2021-05-25 00:56:08 -0300

    Fin sección 4 -Router

---
+++ src/app/auth/auth.routing.ts
@@ -0,0 +1,17 @@
+import { LoginComponent } from './login/login.component';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RegisterComponent } from './register/register.component';
+
+
+
+const routes: Routes = [
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AuthRoutingModule {}
